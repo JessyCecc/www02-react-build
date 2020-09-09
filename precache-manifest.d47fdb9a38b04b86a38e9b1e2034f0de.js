c7eb4d187d462a87d73e41878b16ba5bea0113cd
dev-jc <jean-christophe.ceccaldi@oxatis.com>
2020-09-09 18:19:44 +0200

    First commit

---
+++ precache-manifest.d47fdb9a38b04b86a38e9b1e2034f0de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06eab5649bc401196520bc090285c9a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4963893cd1b6bb34f4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f98e7dc80b9cc629a59",
+    "url": "/static/js/2.6b4a6c43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b4a6c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4963893cd1b6bb34f4",
+    "url": "/static/js/main.1d7d62b7.chunk.js"
+  },
+  {
+    "revision": "37ac6d756ae43cd67b23",
+    "url": "/static/js/runtime-main.4a4cc233.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
